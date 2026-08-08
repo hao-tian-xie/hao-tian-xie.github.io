a13476426be174ebe3f33f6d783c1627ca09bbe6
Tim <tim@TimdeMacBook-Pro.local>
2026-08-08 18:32:50 +0800

    translate publication subtitle

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const links = document.querySelectorAll('a[data-page]');
 const content = document.getElementById('content');
-const pageVersion = '3';
+const pageVersion = '4';
 
 function setActiveLink(page) {
   links.forEach(link => {
